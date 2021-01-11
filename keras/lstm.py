2a5aeae35542750f9c31eb6323b544235d10f626
rkapsalis <volplayer97@hotmail.com>
2021-01-11 02:21:41 +0200

    minor fixes

--- keras/lstm.py
+++ keras/lstm.py
@@ -9,21 +9,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, Dense, LSTM, Dropout
 from tensorflow.python.keras.layers import SpatialDropout1D
 from collections import Counter
-from sklearn.utils import shuffle
 from keras import backend as K
 
 # load dataset
 df = pd.read_csv(r"MoviesDataset.csv")
 print(df.head(10))
-# shuffle the dataset
-df = shuffle(df)
-# rearrange the indexes
-df.reset_index(inplace=True, drop=True)
-print(df.head(10))
 ps = PorterStemmer()
 
 
-def plot_sentiment_histogram(sentiment):  # sentiment histogram
+# create reviews length histogram
+def plot_sentiment_histogram(sentiment):
     unique_vals = df['Sentiment'].sort_values().unique()
     plt.xlabel("Values")
     plt.ylabel("Count")
@@ -33,20 +28,27 @@ def plot_sentiment_histogram(sentiment):  # sentiment histogram
     print(sentiment.values)
 
 
+# plot histogram
 plot_sentiment_histogram(df['Sentiment'])
 
 X = df['Summary'].values
 Y = df['Sentiment'].values
 
+# split dataset in train and test sets
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=42)
+# get the max length of a review (number of words in this sentence)
 max_length = max([len(s.split()) for s in X])
+# get the min length of a review (number of words in this sentence)
 min_length = min([len(s.split()) for s in X])
 print(max_length)
 print(min_length)
 
+# get the length of every review in the dataset
 reviews_len = [len(x.split()) for x in X]
+# create review length histogram
 pd.Series(reviews_len).hist()
+# plot histogram
 plt.show()
 pd.Series(reviews_len).describe()
 # create a sequence of words
@@ -72,7 +74,7 @@ for word in words:
 # sort
 unique.sort()
 
-# print
+# print unique words
 print(len(unique))
 rootWord = []
 for word in unique:
@@ -102,15 +104,15 @@ plt.axis("off")
 plt.show()
 
 max_seq_length = 30
-# convert them into vectors
+# Tokenize sentences, keep 10000 most frequent words
 tokenizer = Tokenizer(num_words=10000)
 # Create the vocabulary index based on word frequency
 tokenizer.fit_on_texts(X)
-# number of the unique words based on the number of elements in this dictionary
+# Get the number of the unique words based on the number of elements in this dictionary
 vocab_size = len(tokenizer.word_index) + 1
 print('Found %s unique tokens.' % vocab_size)
 
-# assign an integer to each word
+# Assign an integer to each word and create integer sequences
 x_train_tokens = tokenizer.texts_to_sequences(X_train)
 x_test_tokens = tokenizer.texts_to_sequences(X_test)
 
@@ -123,43 +125,43 @@ print(X_train.shape)
 print(X_test.shape)
 
 MAX_SEQUENCE_LENGTH = 30
-print(vocab_size)
-EMBEDDING_DIM = 16
+EMBEDDING_DIM = 100
 
-
+# calculate recall
 def recall_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
     recall = true_positives / (possible_positives + K.epsilon())
     return recall
 
+# calculate precision
 def precision_m(y_true, y_pred):
     true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
     predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
     precision = true_positives / (predicted_positives + K.epsilon())
     return precision
 
+# calculate f1-score
 def f1_m(y_true, y_pred):
     precision = precision_m(y_true, y_pred)
     recall = recall_m(y_true, y_pred)
     return 2*((precision*recall)/(precision+recall+K.epsilon()))
 
-#build model
+# build model
 model = Sequential()
 model.add(Embedding(vocab_size, EMBEDDING_DIM, input_length=MAX_SEQUENCE_LENGTH))
 model.add(SpatialDropout1D(0.5))
 model.add(LSTM(50, dropout=0.5, recurrent_dropout=0.5))
 model.add(Dropout(0.5))
-
 model.add(Dense(1, activation='sigmoid'))
-
 model.compile(loss='binary_crossentropy', optimizer='Adam', metrics=['accuracy', f1_m, precision_m, recall_m])
 model.summary()
 
 history = model.fit(X_train, y_train, epochs=5, validation_data=(X_test, y_test), verbose=True, batch_size=32)
 loss, accuracy, f1_score, precision, recall = model.evaluate(X_test, y_test, verbose=1)
 # scores = model.evaluate(X_test, y_test, verbose=1)
-# print("Accuracy: %.2f%%" % (scores[1]*100))
+
+# Print metrics
 print("F1-score")
 print(f1_score)
 print("Precision")
@@ -187,20 +189,31 @@ plt.legend(['train', 'test'], loc='upper left')
 plt.show()
 plt.savefig('foo1.png')
 
+# Test our model
 test_word = "This is a bad bad movie"
 tw = tokenizer.texts_to_sequences([test_word])
 tw = pad_sequences(tw, maxlen=30)
+print(model.predict(tw))
 prediction = int(model.predict(tw).round().item())
 print(Y[prediction])
 
 test_word = "This film is terrible"
-tw = tokenizer.texts_to_sequences([test_word])
-tw = pad_sequences(tw, maxlen=30)
-prediction = int(model.predict(tw).round().item())
-print(Y[prediction])
+tw1 = tokenizer.texts_to_sequences([test_word])
+tw1 = pad_sequences(tw1, maxlen=30)
+print(model.predict(tw1))
+prediction1 = int(model.predict(tw1).round().item())
+print(Y[prediction1])
 
 test_word = "This film is great"
-tw = tokenizer.texts_to_sequences([test_word])
-tw = pad_sequences(tw, maxlen=30)
-prediction = int(model.predict(tw).round().item())
-print(Y[prediction])
+tw2 = tokenizer.texts_to_sequences([test_word])
+tw2 = pad_sequences(tw2, maxlen=30)
+print(model.predict(tw2))
+prediction2 = int(model.predict(tw2).round().item())
+print(Y[prediction2])
+
+test_word = "This film is awesome"
+tw3 = tokenizer.texts_to_sequences([test_word])
+tw3 = pad_sequences(tw3, maxlen=30)
+print(model.predict(tw3))
+prediction3 = int(model.predict(tw3).round().item())
+print(Y[prediction3])
